b3b0958947c2c95fa3134727a975b3c5555c8a4f
ketillyqueiroz <queirozketilly0@gmail.com>
2025-04-26 12:05:48 -0300

    Email random

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress"/>
 // traz todos os métodos e funções do cypress
-
 describe('Funcionalidade: Cadastro de membros', () => {
   it('Deve fazer o cadastro de campos obrigatórios', () => {
+    const email = `user${Date.now()}@teste.com`
+
     cy.visit('http://127.0.0.1:8080/')
-    cy.get('#signup-firstname').type('Jose')
-    cy.get('#signup-lastname').type('Miguel')
-    cy.get('#signup-email').type('jmiguel1293@teste.com')
+    cy.get('#signup-firstname').type('José')
+    cy.get('#signup-lastname').type('Eduardo')
+    cy.get('#signup-email').type(email)
     cy.get('#signup-phone').type('83997459744')
     cy.get('#signup-password').type('Password@1234')
     cy.get('#signup-button').click()
